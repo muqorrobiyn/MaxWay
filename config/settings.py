d7c0ee3af982123a1d23abe14d03394433c36ddc
SMS3384 <akkuntsms8@gmail.com>
2025-01-04 19:55:49 +0500

    intial commit migrations (o'zi buni yozish nimaga kerak tushunmadim)

--- config/settings.py
+++ config/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME','max_way_db'),
-        'USER':os.getenv('DB_USER','max_way_user'),
-        'PASSWORD':os.getenv('DB_PASSWORD','root'),
-        'HOST':os.getenv('DB_HOST','127.0.0.1'),
-        'PORT':os.getenv('DB_PORT','5432'),
+        'NAME': 'maxwaydb',
+        'USER':'maxwayuser',
+        'PASSWORD': 'user',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
